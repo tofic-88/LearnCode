393843ef42852e0159658f2068ac16ba5d27ac09
Tofic <mehrazitawfik@gmail.com>
2021-06-07 15:53:12 -0400

    CSS Float Layout/ training

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
 <link rel="stylesheet" href="style.css">
 <body>
 
-<h1>This is a heading</h1>
-<p>This is a paragraph.</p>
+<header><h3>This is the Header </h3></header>
+  <div class="clearfix">
+          <div class="column menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          </div>
 
-<p>This is a small message to understand that we going to be the best programmor for ever in montreal island</p>
+          <div class="column content">
+          <h1>Building clearfix layout</h1>
+          <p>This is training about float and clear property</p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            semper auctor neque vitae tempus quam pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque. Egestas congue
+              quisque egestas diam in arcu cursus euismod. Facilisi cras fermentum odio. Sit amet dictum sit amet. Non curabitur gravida arcu ac tortor dignissim convallis. Rhoncus urna neque viverra justo nec ultrices.
+            Dictum sit amet justo donec enim diam vulputate ut. Felis t dolor. Nulla facilisi morbi tempus iaculis urna.</p>
+          </div>
+  </div>
+<footer><h3>This is the footer</h3></footer>
 
 </body>
 </html>
