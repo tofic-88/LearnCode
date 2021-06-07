32af7066c74d199fc8f2ecfe82243943cf9cef75
Tofic <mehrazitawfik@gmail.com>
2021-06-07 13:53:07 -0400

    just lanch the first page index.php as start page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>Learn Code</title>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+<p>This is a small message to understand that we going to be the best programmor for ever in montreal island</p>
+
+</body>
+</html>
